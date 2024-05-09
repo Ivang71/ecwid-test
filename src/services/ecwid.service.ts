5354f389b468afb9e811af8bbaaee595dfb6dbed
Ivang71 <artemtolv@gmail.com>
2024-05-09 17:24:49 +0500

    fixes to conform to the task

--- src/services/ecwid.service.ts
+++ src/services/ecwid.service.ts
@@ -1,59 +1,58 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const storeId = '58958138';
-const baseUrl = `https://app.ecwid.com/api/v3/${storeId}`;
-const token = 'public_7BxbJGWyDaZfSQqjVS5Ftr4jzXkS43UD';
+const storeId = '58958138'
+const baseUrl = `https://app.ecwid.com/api/v3/${storeId}`
+const token = 'public_7BxbJGWyDaZfSQqjVS5Ftr4jzXkS43UD'
 const ecwidApi = axios.create({
-  baseURL: baseUrl,
-  headers: {
-    "Authorization" : `Bearer ${token}`
-  }
-});
+    baseURL: baseUrl,
+    headers: {
+        "Authorization": `Bearer ${token}`
+    }
+})
 
 export const getCategories = async () => {
-  const response = await ecwidApi.get('/categories');
-  return response.data.items;
-};
+    const response = await ecwidApi.get('/categories')
+    return response.data.items
+}
 
 export const getCategoryInfo = async (categoryId: number) => {
-  const response = await ecwidApi.get(`/categories/${categoryId}`);
-  return response.data;
-};
+    const response = await ecwidApi.get(`/categories/${categoryId}`)
+    return response.data
+}
 
 export const getProductsByCategory = async (categoryId: number) => {
-  const url = `${baseUrl}/products?categories=${categoryId}`;
-  const response = await ecwidApi.get(url);
-  return response.data.items;
-};
+    const url = `${baseUrl}/products?categories=${categoryId}`
+    const response = await ecwidApi.get(url)
+    return response.data.items
+}
 
 export const getProductDetails = async (productId: number) => {
-  const url = `${baseUrl}/products/${productId}`;
-  const response = await ecwidApi.get(url);
-  return response.data;
-};
+    const url = `${baseUrl}/products/${productId}`
+    const response = await ecwidApi.get(url)
+    return response.data
+}
 
 // Add product to local storage cart
 export const addToCart = (product: any) => {
-  const cart = getCartItems();
-  console.log(cart)
-  cart.push(product);
-  localStorage.removeItem('cart')
-  localStorage.setItem('cart', JSON.stringify(cart));
-};
+    const cart = getCartItems()
+    cart.push(product)
+    localStorage.removeItem('cart')
+    localStorage.setItem('cart', JSON.stringify(cart))
+}
 
 // Get items from local storage cart
 export const getCartItems = (): any[] => {
-  const cartString = localStorage.getItem('cart');
-  return cartString ? JSON.parse(cartString) : [];
-};
+    const cartString = localStorage.getItem('cart')
+    return cartString ? JSON.parse(cartString) : []
+}
 
 // Remove item from local storage cart
 export const removeFromCart = (productId: number) => {
-  const cart = getCartItems();
-  const updatedCart = cart.filter((item: any) => item.id !== productId);
-  localStorage.removeItem('cart')
-  localStorage.setItem('cart', JSON.stringify(updatedCart));
-};
+    const cart = getCartItems()
+    const updatedCart = cart.filter((item: any) => item.id !== productId)
+    localStorage.removeItem('cart')
+    localStorage.setItem('cart', JSON.stringify(updatedCart))
+}
 
 export const clearCart = () => {
     localStorage.removeItem('cart')
--- src/views/CategoryDetailsView.vue
+++ src/views/CategoryDetailsView.vue
@@ -1,42 +1,47 @@
 <template>
-  <div v-if="category">
-    <h2>{{ category.name }}</h2>
-    <img v-if="category.image" :src="category.imageUrl" alt="" />
-    <ul v-if="products.length > 0">
-      <li v-for="product in products" :key="product.id">
-        <a :href="`/product/${product.id}`">
-          {{ product.name }}
-          <span v-if="product.price"> - {{ product.price.text }}</span>
-        </a>
-      </li>
-    </ul>
-    <p v-else>No products found in this category.</p>
-  </div>
-  <div v-else>
-    <p>Loading category details...</p>
-  </div>
+    <div v-if="category">
+        <h2>{{ category.name }}</h2>
+        <img v-if="category.image" :src="category.imageUrl" alt="" />
+        <ul v-if="products.length > 0">
+            <li v-for="product in products" :key="product.id">
+                <img v-if="product.hdThumbnailUrl" :src="product.hdThumbnailUrl" alt="" />
+                {{ product.name }}
+                <span v-if="product.defaultDisplayedPriceFormatted"> {{ product.defaultDisplayedPriceFormatted}}</span>
+                <button @click="buy(product)">Buy</button>
+            </li>
+        </ul>
+        <p v-else>No products found in this category.</p>
+    </div>
+    <div v-else>
+        <p>Loading category details...</p>
+    </div>
 </template>
 
 <script>
 import { getCategoryInfo, getProductsByCategory } from '@/services/ecwid.service'
 
 export default {
-  data() {
-    return {
-      categoryId: this.$route.params.id,
-      category: null,
-      products: [],
+    data() {
+        return {
+            categoryId: this.$route.params.id,
+            category: null,
+            products: [],
+        }
+    },
+    async created() {
+        if (this.categoryId) {
+            [this.category, this.products] = await Promise.all([
+                getCategoryInfo(this.categoryId),
+                getProductsByCategory(this.categoryId)
+            ])
+        }
+    },
+    methods: {
+        buy(product) {
+            this.$router.push(`/product/${product.id}`)
+        }
     }
-  },
-  async created() {
-    if (this.categoryId) {
-      [this.category, this.products] = await Promise.all([
-        getCategoryInfo(this.categoryId),
-        getProductsByCategory(this.categoryId)
-      ])
-    }
-  },
-};
+}
 </script>
 
 <style scoped>
