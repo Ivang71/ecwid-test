66d3c3d3bf6b484b754a3b1b8fce53b2ade41e63
Ivang71 <artemtolv@gmail.com>
2024-05-10 10:57:41 +0500

    create navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+    <nav
+        class="navbar absolute left-0 top-0 right-0 flex items-center justify-between px-4 py-2 bg-gray-500 text-white">
+        <div class="flex items-center">
+            <span class="text-xl font-bold">Ecwid</span>
+        </div>
+        <div class="flex items-center">
+            <button @click="openCart"
+                class="flex items-center px-2 py-1 rounded-md hover:bg-gray-600 focus:outline-none">
+                <span v-if="cartItemCount > 0"
+                    class="text-xs font-bold ml-1 bg-red-500 text-white rounded-full px-1 py-0.5">{{ cartItemCount }}
+                </span>
+                <img src="@/assets/static/cart.svg" width="40px"/>
+            </button>
+        </div>
+    </nav>
+</template>
+
+<script>
+import { getCartItems } from '@/services/ecwid.service';
+export default {
+    data() {
+        return {
+            cartItemCount: getCartItems().length,
+        }
+    },
+    mounted() {
+        this.updateCartItemCount();
+        this.$root.$on('cartUpdated', this.updateCartItemCount); // Listen for the event
+    },
+    beforeUnmount() {
+        this.$root.$off('cartUpdated', this.updateCartItemCount); // Remove the listener
+    },
+    methods: {
+        openCart() {
+            this.$router.push('/cart')
+        },
+        updateCartItemCount() {
+            this.cartItemCount = getCartItems().length; // Update cart items count
+        },
+    },
+};
+</script>
